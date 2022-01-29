3238369c9e4558efbd4851062e4d225ffa6503a5
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-01-29 17:51:59 +0000

    [iiw20, es120] Outline skeleton of lexer, define parsers for char, nats and bools

--- src/main/scala/frontend/lexer.scala
+++ src/main/scala/frontend/lexer.scala
@@ -1,10 +1,16 @@
 package frontend
 
-import parsley.character.isWhitespace
+import parsley.Parsley
+import parsley.Parsley._
+import parsley.character.{digit, isWhitespace}
+import parsley.combinator.eof
+import parsley.errors.combinator.ErrorMethods
 import parsley.token.{LanguageDef, Lexer, Predicate}
+import parsley.implicits.character.stringLift
+
+import scala.language.implicitConversions
 
 object lexer {
-  //noinspection ScalaStyle
   private val wacc = LanguageDef.plain.copy(
     commentLine = "#",
     keywords = Set("begin", "end", "skip", "read", "free", "return", "exit", "print", "println",
@@ -17,5 +23,28 @@ object lexer {
     space = Predicate(isWhitespace)
   )
 
-  new Lexer(wacc)
+  val lexer = new Lexer(wacc)
+
+  // Identifiers for variables and functions
+  val ID = lexer.identifier.filterOut {
+    case k if wacc.keywords(k) => s"keyword $k cannot be used as an identifier"
+  }
+
+  val NAT = digit.foldLeft1[Int](0)((n, d) => n * 10 + d.asDigit)
+  // TODO: negative numbers - sequence an optional +/- before the digits
+  val INT = ???
+  val BOOL = "true" #> true <|> "false" #> false
+  val CHAR = lexer.charLiteral
+  val STRING = ???
+
+  def fully[A](p: Parsley[A]): Parsley[A] = lexer.whiteSpace ~> p <~ eof
+
+  object implicits {
+    implicit def implicitToken(s: String): Parsley[Unit] = {
+      if (wacc.keywords(s))       lexer.keyword(s)
+      else if (wacc.operators(s)) lexer.maxOp(s)
+      // check why we need symbol_ not symbol
+      else                        void(lexer.symbol_(s))
+    }
+  }
 }
