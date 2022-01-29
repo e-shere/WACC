7d324c0c99f5eb31dd7cbbfefa2a6b6b119a25dc
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-01-29 17:56:20 +0000

    [iiw20, es120] Style changes

--- src/main/scala/frontend/lexer.scala
+++ src/main/scala/frontend/lexer.scala
@@ -5,46 +5,45 @@ import parsley.Parsley._
 import parsley.character.{digit, isWhitespace}
 import parsley.combinator.eof
 import parsley.errors.combinator.ErrorMethods
-import parsley.token.{LanguageDef, Lexer, Predicate}
 import parsley.implicits.character.stringLift
+import parsley.token.{LanguageDef, Lexer, Predicate}
 
 import scala.language.implicitConversions
 
 object lexer {
-  private val wacc = LanguageDef.plain.copy(
-    commentLine = "#",
-    keywords = Set("begin", "end", "skip", "read", "free", "return", "exit", "print", "println",
-                    "if", "then", "else", "fi", "while", "do", "done", "newpair", "call",
-                    "fst", "snd", "int", "bool", "char", "string", "pair", "true", "false", "null"),
-    operators = Set("!", "-", "len", "ord", "chr", "*", "/", "%", "+",
-                    ">", "<", ">=", "<=", "==", "!=", "&&", "||"),
-    identStart = Predicate(c => c.isLetter || c == '_'),
-    identLetter = Predicate(c => c.isLetterOrDigit || c == '_'),
-    space = Predicate(isWhitespace)
-  )
-
-  val lexer = new Lexer(wacc)
-
   // Identifiers for variables and functions
   val ID = lexer.identifier.filterOut {
     case k if wacc.keywords(k) => s"keyword $k cannot be used as an identifier"
   }
-
   val NAT = digit.foldLeft1[Int](0)((n, d) => n * 10 + d.asDigit)
   // TODO: negative numbers - sequence an optional +/- before the digits
   val INT = ???
   val BOOL = "true" #> true <|> "false" #> false
   val CHAR = lexer.charLiteral
   val STRING = ???
+  private val wacc = LanguageDef.plain.copy(
+    commentLine = "#",
+    // @formatter:off
+    keywords = Set("begin", "end", "skip", "read", "free", "return", "exit", "print", "println",
+                    "if", "then", "else", "fi", "while", "do", "done", "newpair", "call",
+                    "fst", "snd", "int", "bool", "char", "string", "pair", "true", "false", "null"),
+    operators = Set("!", "-", "len", "ord", "chr", "*", "/", "%", "+",
+                    ">", "<", ">=", "<=", "==", "!=", "&&", "||"),
+    // @formatter:on
+    identStart = Predicate(c => c.isLetter || c == '_'),
+    identLetter = Predicate(c => c.isLetterOrDigit || c == '_'),
+    space = Predicate(isWhitespace)
+  )
+  private val lexer = new Lexer(wacc)
 
   def fully[A](p: Parsley[A]): Parsley[A] = lexer.whiteSpace ~> p <~ eof
 
   object implicits {
     implicit def implicitToken(s: String): Parsley[Unit] = {
-      if (wacc.keywords(s))       lexer.keyword(s)
+      if (wacc.keywords(s)) lexer.keyword(s)
       else if (wacc.operators(s)) lexer.maxOp(s)
       // check why we need symbol_ not symbol
-      else                        void(lexer.symbol_(s))
+      else void(lexer.symbol_(s))
     }
   }
 }
