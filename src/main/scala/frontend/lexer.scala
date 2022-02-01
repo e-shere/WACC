083c653f094c72de13cece356c63072df0359dbf
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-02-01 15:24:51 +0000

    [es120] Added type annonations and removed todo comments

--- src/main/scala/frontend/lexer.scala
+++ src/main/scala/frontend/lexer.scala
@@ -2,7 +2,7 @@ package frontend
 
 import parsley.Parsley
 import parsley.Parsley._
-import parsley.character.{digit, isWhitespace, noneOf, oneOf}
+import parsley.character.{digit, isWhitespace, noneOf}
 import parsley.combinator.{choice, eof, many, optionally}
 import parsley.implicits.character.{charLift, stringLift}
 import parsley.token.{LanguageDef, Lexer, Predicate}
@@ -10,17 +10,13 @@ import parsley.token.{LanguageDef, Lexer, Predicate}
 import scala.language.implicitConversions
 
 object lexer {
-
-  
   private val wacc = LanguageDef.plain.copy(
     commentLine = "#",
-    // @formatter:off
     keywords = Set("begin", "end", "skip", "read", "free", "return", "exit", "print", "println",
                     "if", "then", "else", "fi", "while", "do", "done", "newpair", "call",
                     "fst", "snd", "int", "bool", "char", "string", "pair", "true", "false", "null"),
     operators = Set("!", "-", "len", "ord", "chr", "*", "/", "%", "+",
                     ">", "<", ">=", "<=", "==", "!=", "&&", "||"),
-    // @formatter:on
     identStart = Predicate(c => c.isLetter || c == '_'),
     identLetter = Predicate(c => c.isLetterOrDigit || c == '_'),
     space = Predicate(isWhitespace)
@@ -28,30 +24,26 @@ object lexer {
   private val lexer = new Lexer(wacc)
   
   // Identifiers for variables and functions
-  val ID = lexer.identifier
+  val ID: Parsley[String] = lexer.identifier
   private def token[A](p: =>Parsley[A]): Parsley[A] = {
     lexer.lexeme(attempt(p))
   }
 
-  // include negation token then negation legal only if it is NOT immediately followed by an integer
-  // ie needs to consume whitespace/bracket/smth not an integer
-  // same for plus
-  val NEG = token('-' *> notFollowedBy(digit))
+  val NEG: Parsley[Unit] = token('-' *> notFollowedBy(digit))
 
   private val minus: Parsley[Int => Int] = '-' #> {x: Int => -x}
   private val plus: Parsley[Int => Int] = optionally('+', identity)
   private val nat = digit.foldLeft1[Int](0)((n, d) => n * 10 + d.asDigit)
-  // need to make sign optional
-  val INT = token((minus <|> plus) <*> nat)
+  val INT: Parsley[Int] = token((minus <|> plus) <*> nat)
 
-  val BOOL = token("true" #> true <|> "false" #> false)
+  val BOOL: Parsley[Boolean] = token("true" #> true <|> "false" #> false)
 
   private val escapeChar =
     choice('0' #> '\0', 'b' #> '\b', 't' #> '\t', 'f' #> '\f', 'r' #> '\r', '\"', '\'', '\\')
-  private val charletter = noneOf('\\', '\'', '\"') <|> ('\\' *> escapeChar)
-  val CHAR = token('\'' *> charletter <* '\'')
+  private val charLetter = noneOf('\\', '\'', '\"') <|> ('\\' *> escapeChar)
+  val CHAR: Parsley[Char] = token('\'' *> charLetter <* '\'')
 
-  val STRING = token('\"' *> many(charletter).map(_.mkString) <* '\"')
+  val STRING: Parsley[String] = token('\"' *> many(charLetter).map(_.mkString) <* '\"')
 
   def fully[A](p: Parsley[A]): Parsley[A] = lexer.whiteSpace ~> p <~ eof
 
