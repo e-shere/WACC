3f91025fd035d7c3d080d62afa5e395d61bb378f
Robert Weingart <robert.weingart20@imperial.ac.uk>
2022-02-03 11:57:48 +0000

    [raw20] Fix issue with precedence table

--- src/main/scala/frontend/parser.scala
+++ src/main/scala/frontend/parser.scala
@@ -5,6 +5,7 @@ import ast._
 import parsley.Parsley, parsley.Parsley._
 import parsley.expr.{InfixL, Prefix, SOps, precedence}
 import parsley.implicits.character.{charLift, stringLift}
+import parsley.expr.Atoms
 
 //import lexer.implicits.implicitToken
 //import parsley.errors.ErrorBuilder
@@ -15,17 +16,21 @@ import parsley.implicits.character.{charLift, stringLift}
 object parser {
     
     private lazy val `<expr>`: Parsley[Expr] =
-        precedence(SOps(InfixL)(Or        <# "||" ),
-                   SOps(InfixL)(And       <# "&&"),
-                   SOps(InfixL)(Eq   <# "==", Neq     <# "!="),
-                   SOps(InfixL)(Lt    <# "<",  Leq    <# "<=",
-                                Gt <# ">",  Geq <# ">="),
-                   SOps(InfixL)(Add       <# "+",  Sub          <# "-"),
-                   SOps(InfixL)(Mul       <# "*",  Div          <# "/",
-                                Mod       <# "%"),
-                   SOps(Prefix)(Neg    <# "-",  Not          <# "!",
-                                Len       <# "len", Ord         <# "ord",
-                                Chr       <# "chr")  ) //,  `<expr-10>`)
+        precedence(SOps(InfixL)(Or  <# "||" ) +:
+                   SOps(InfixL)(And <# "&&") +:
+                   SOps(InfixL)(Eq  <# "==", Neq <# "!=") +:
+                   SOps(InfixL)(Lt  <# "<",  Leq <# "<=",
+                                Gt  <# ">",  Geq <# ">=") +:
+                   SOps(InfixL)(Add <# "+",  Sub <# "-") +:
+                   SOps(InfixL)(Mul <# "*",  Div <# "/", Mod <# "%") +:
+                   SOps(Prefix)(Neg <# "-",  Not <# "!",
+                                Len <# "len", Ord <# "ord",
+                                Chr <# "chr") +:
+                   `<expr0>`)
+
+    private lazy val `<expr0>` = Atoms(
+        
+    )
 
 
 }
