5a75ac8d2d360cccea1baac01ba4719795caada6
Ines W <ineswright7@gmail.com>
2022-02-03 11:31:01 +0000

    [es120, iiw20, ki20] Create precedence table, unresolved typing issues

---
+++ src/main/scala/frontend/parser.scala
@@ -0,0 +1,31 @@
+package frontend
+
+import lexer._
+import ast._
+import parsley.Parsley, parsley.Parsley._
+import parsley.expr.{InfixL, Prefix, SOps, precedence}
+import parsley.implicits.character.{charLift, stringLift}
+
+//import lexer.implicits.implicitToken
+//import parsley.errors.ErrorBuilder
+//import parsley.Result
+//import parsley.debug._
+//import parsley.combinator.{sepBy1, many, some}
+
+object parser {
+    
+    private lazy val `<expr>`: Parsley[Expr] =
+        precedence(SOps(InfixL)(Or        <# "||" ),
+                   SOps(InfixL)(And       <# "&&"),
+                   SOps(InfixL)(Eq   <# "==", Neq     <# "!="),
+                   SOps(InfixL)(Lt    <# "<",  Leq    <# "<=",
+                                Gt <# ">",  Geq <# ">="),
+                   SOps(InfixL)(Add       <# "+",  Sub          <# "-"),
+                   SOps(InfixL)(Mul       <# "*",  Div          <# "/",
+                                Mod       <# "%"),
+                   SOps(Prefix)(Neg    <# "-",  Not          <# "!",
+                                Len       <# "len", Ord         <# "ord",
+                                Chr       <# "chr")  ) //,  `<expr-10>`)
+
+
+}
