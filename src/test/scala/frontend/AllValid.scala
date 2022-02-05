155cba60c0bfdf3a7641594aa84a0f015af5a37e
Ines W <ineswright7@gmail.com>
2022-02-05 21:42:56 +0000

    [iiw20] Include tests for success of every file in every directory of valid examples

--- src/test/scala/frontend/AllValid.scala
+++ src/test/scala/frontend/AllValid.scala
@@ -25,10 +25,125 @@ class AllValid extends AnyFlatSpec {
         }
     }
 
-    "All basic valid programs" should "return success" in {
-        val srcPath = "src/examples/valid/basic/skip"
+    // TODO: ISSUE WITH PAIRS
+    "All valid advanced programs" should "return success" in {
+        val srcPath = "src/examples/valid/advanced"
         val allValidProgramPaths = getListOfFilesRecursively(srcPath)
-        info(allValidProgramPaths.mkString)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    // TODO: DOESN'T RECOGNISE []
+    "All valid array programs" should "return success" in {
+        val srcPath = "src/examples/valid/array"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    "All valid basic programs" should "return success" in {
+        val srcPath = "src/examples/valid/basic"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    "All valid expressions programs" should "return success" in {
+        val srcPath = "src/examples/valid/expressions"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    // TODO: DOESN'T RECOGNISE '('
+        "All valid function programs" should "return success" in {
+            val srcPath = "src/examples/valid/function"
+            val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+            for (path <- allValidProgramPaths) {
+                val source = Source.fromFile(path).mkString
+                parse(source) should matchPattern { case Success(_) => }
+            }
+        }
+
+    "All valid if programs" should "return success" in {
+        val srcPath = "src/examples/valid/if"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    // TODO: DOESN'T RECOGNISE \n
+    "All valid IO programs" should "return success" in {
+        val srcPath = "src/examples/valid/IO"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    "All valid pairs programs" should "return success" in {
+        val srcPath = "src/examples/valid/pairs"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    // fails because doesn't recognise array []
+    "All valid runtimeErr programs" should "return success" in {
+        val srcPath = "src/examples/valid/runtimeErr"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    // fails because doesn't recognise array []
+    "All valid scope programs" should "return success" in {
+        val srcPath = "src/examples/valid/scope"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    // fails because of pairs
+    "All valid sequence programs" should "return success" in {
+        val srcPath = "src/examples/valid/sequence"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    // fails because of pairs
+    "All valid variables programs" should "return success" in {
+        val srcPath = "src/examples/valid/variables"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
+        for (path <- allValidProgramPaths) {
+            val source = Source.fromFile(path).mkString
+            parse(source) should matchPattern { case Success(_) => }
+        }
+    }
+
+    "All valid while programs" should "return success" in {
+        val srcPath = "src/examples/valid/while"
+        val allValidProgramPaths = getListOfFilesRecursively(srcPath)
         for (path <- allValidProgramPaths) {
             val source = Source.fromFile(path).mkString
             parse(source) should matchPattern { case Success(_) => }
@@ -36,12 +151,6 @@ class AllValid extends AnyFlatSpec {
     }
 
 
-    // class SuccessMatcher extends BeMatcher[Int] {
-    //     def apply(result:  Result[ErrorBuilder[String], WaccProgram]) =
-    //         MatchResult(
-    //             left % 2 == 1,
-    //             left.toString + " was even",
-    //             left.toString + " was odd"
-    //         )
-    // }
+
+
 }
