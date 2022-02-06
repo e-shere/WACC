017db4d71f1b047109b1a63cf115e7826e04c846
Ines W <ineswright7@gmail.com>
2022-02-06 21:02:51 +0000

    [iiw20, raw20] Complete return in semantic analysis

--- src/main/scala/frontend/semanticChecker.scala
+++ src/main/scala/frontend/semanticChecker.scala
@@ -17,21 +17,21 @@ object semanticChecker {
             id->FuncType(ty, args.map{case Param(ty, _) => ty})
           }
         }.toMap
-        for (Func(_, _, args, body) <- funcs) {
+        for (Func(ty, _, args, body) <- funcs) {
           val argsTable: Map[Ident, Type] = args.map {
             case Param(ty, id) => id->ty
           }.toMap
-          validateBlock(funcTable, argsTable, body)
-
+          validateBlock(funcTable, argsTable, body, Some(ty))
         }
-        validateBlock(funcTable, Map.empty[Ident, Type], stats)
+        validateBlock(funcTable, Map.empty[Ident, Type], stats, None)
       }
     }
   }
 
   def validateBlock(funcTable: Map[Ident, FuncType],
                     parentSymbols: Map[Ident, Type],
-                    stats: List[Stat]): List[SemanticError] = {
+                    stats: List[Stat],
+                    returnType: Option[Type]): List[SemanticError] = {
     val errors: mutable.ListBuffer[SemanticError] = mutable.ListBuffer.empty
     val localSymbols: mutable.Map[Ident, Type] = mutable.Map.empty[Ident, Type]
     for (stat <- stats) {
@@ -78,7 +78,15 @@ object semanticChecker {
           }
         }
         case Return(expr) => {
-          // todo
+          val (maybeExpr, exprErrors) = validateExpr(funcTable, localSymbols.toMap ++ parentSymbols, expr)
+          errors ++= exprErrors
+          (maybeExpr, returnType) match {
+            case (Some(exprType), Some(ty)) => if (exprType != ty) {
+              errors += SemanticError("return type must match return type of function")
+            }
+            case (_, None) => errors += SemanticError("can't return outside a function")
+            case (None, Some(_)) => {}
+          }
         }
         case Exit(expr) => {
           val (maybeExpr, exprErrors) = validateExpr(funcTable, localSymbols.toMap ++ parentSymbols, expr)
@@ -107,8 +115,8 @@ object semanticChecker {
             }
             case _ => {}
           }
-          errors ++= validateBlock(funcTable, localSymbols.toMap ++ parentSymbols, thenStats)
-          errors ++= validateBlock(funcTable, localSymbols.toMap ++ parentSymbols, elseStats)
+          errors ++= validateBlock(funcTable, localSymbols.toMap ++ parentSymbols, thenStats, returnType)
+          errors ++= validateBlock(funcTable, localSymbols.toMap ++ parentSymbols, elseStats, returnType)
         }
         case While(expr, doStats) => {
           val (maybeExpr, exprErrors) = validateExpr(funcTable, localSymbols.toMap ++ parentSymbols, expr)
@@ -120,15 +128,14 @@ object semanticChecker {
             }
             case _ => {}
           }
-          errors ++= validateBlock(funcTable, localSymbols.toMap ++ parentSymbols, doStats)
+          errors ++= validateBlock(funcTable, localSymbols.toMap ++ parentSymbols, doStats, returnType)
         }
         case Scope(innerStats) => {
-          errors ++= validateBlock(funcTable, localSymbols.toMap ++ parentSymbols, innerStats)
+          errors ++= validateBlock(funcTable, localSymbols.toMap ++ parentSymbols, innerStats, returnType)
         }
       }
     }
     errors.toList
-
   }
 
   def validateExpr(funcTable: Map[Ident, FuncType],
