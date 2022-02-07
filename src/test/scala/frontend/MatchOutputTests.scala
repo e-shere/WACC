3161a5e0227d0887447587cea0016cd0b0461965
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-02-07 14:29:44 +0000

    [es120] Fixed expected AST structure for skip

--- src/test/scala/frontend/MatchOutputTests.scala
+++ src/test/scala/frontend/MatchOutputTests.scala
@@ -17,6 +17,6 @@ class MatchOutputTests extends AnyFlatSpec {
     "begin skip end program" should "only skip" in {
         val source = Source.fromFile("src/examples/valid/basic/skip/skip.wacc").mkString
         //parse(source).toString() shouldBe "Success(WaccProgram(List(),List(Skip((8,7)))))"
-      parse(new File("src/examples/valid/basic/skip/skip.wacc")).toString() shouldBe "Success(WaccProgram(List(),List(Skip((8,7)))))"
+      parse(new File("src/examples/valid/basic/skip/skip.wacc")).toString() shouldBe "Success(WaccProgram(List(),List(Skip()(8,7)))(8,7))"
     }
 }
