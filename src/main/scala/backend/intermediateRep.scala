1167e749999db30204358c8052d1a00c551afe74
Ines W <ineswright7@gmail.com>
2022-02-16 23:33:11 +0000

    [iiw20, raw20] Typedefs for backend symbol table£

--- src/main/scala/backend/intermediateRep.scala
+++ src/main/scala/backend/intermediateRep.scala
@@ -1,8 +1,14 @@
 package backend
 
-class ir {
+import frontend.ast._
 
-  
+class intermediateRep {
+
+  type SymbolTable = Map[(ScopeId, Ident), SymbolEntry]
+
+  type ScopeId = (Int, Int)
+
+  case class SymbolEntry(ty: Type, start: Int, end: Int)
 
 
 }
