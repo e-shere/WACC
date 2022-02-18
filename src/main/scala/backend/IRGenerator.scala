70180930a3f76898b2989aad7b030ac71245ecfa
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-02-18 20:00:50 +0000

    [es120] move logic of function structure to ir generator from ir, abstract parts of ir generator, add code generation test

--- src/main/scala/backend/IRGenerator.scala
+++ src/main/scala/backend/IRGenerator.scala
@@ -5,18 +5,33 @@ import backend.intermediateRep._
 import java.io._
 
 object IRGenerator {
+  type IRFunc = List[IRNode]
 
-  def generateIR(ast: WaccProgram): List[FUNCTION] = {
-    ast.funcs.map(f => FUNCTION(f.id.id, generateBody(f.body))) :+ FUNCTION("main", generateBody(ast.stats) :+ RETURN())
+  def generateIR(ast: WaccProgram): List[IRFunc] = {
+    ast.funcs.map(f => generateFunc(f.id.id, f.body)) :+ generateFunc("main", ast.stats)
   }
 
-  def generateBody(stats: List[Stat]): List[IRNode] = {
+  def generateFunc(name: String, body: List[Stat]): IRFunc = {
+    LABEL(name) +:
+    START_FUNC() +:
+    generateBlock(body) :+
+    RETURN(0) :+
+    EXIT_FUNC()
+  }
+
+  def generateBlock(stats: List[Stat]): List[IRNode] = {
     stats match {
         // TODO: generate assembly needed to evaluate expr, then insert something referring to that
         // into the return IR node- perhaps an intermediate value with register in the symbol table
-      case List(Return(_)) => List(RETURN())
-      case _ :: t => generateBody(t)
-      case Nil => List.empty // WILL NEVER REACH HERE!
+      case Nil => List.empty
+      case Skip() :: tail => generateBlock(tail)
+      case stat :: tail => generateStat(stat) +: generateBlock(tail)
+    }
+  }
+
+  def generateStat(stat: Stat): IRNode = {
+    stat match {
+      case Return(_) => RETURN(0)
     }
   }
 
@@ -25,9 +40,8 @@ object IRGenerator {
 
     val pw = new PrintWriter(new File(outputPath + ".s"))
     pw.write(".text\n\n.global main\n")
-
-    pw.write(rep.mkString("\n") + "\n")
-    pw.close
+    rep.foreach(f => pw.write(f.mkString("\n") + "\n"))
+    pw.close()
 
   }
 
--- src/main/scala/backend/intermediateRep.scala
+++ src/main/scala/backend/intermediateRep.scala
@@ -12,12 +12,21 @@ object intermediateRep {
   //TODO: load will need to change to put the return value in r0, not just always return 0
   // push {lr} stores the return address of where the function was called from
   // pop {pc} pulls that back out so control is returned to the caller
-  case class FUNCTION(name: String, body: List[IRNode]) {
-    override def toString = name + ":\n\tPUSH {lr}\n" + body.mkString("\n") +
-      "\n\tPOP {pc}\n\t.ltorg"
+  case class LABEL(label: String) extends IRNode {
+    override def toString: String = s"${label}:"
   }
-  case class RETURN() extends IRNode {
-    override def toString = "\tLDR r0, =0"
+  case class START_FUNC() extends IRNode {
+    override def toString = "PUSH {lr}"
+  }
+  case class EXIT_FUNC() extends IRNode {
+    override def toString = "POP {pc}\n.ltorg"
+  }
+  case class LOAD_ARG() extends IRNode {
+    override def toString = ""
+  }
+
+  case class RETURN(exitCode: Int) extends IRNode {
+    override def toString = s"LDR r0, =${exitCode}"
   }
 
 }
