1bca7e088e9c8e765f3ac8606238b8e05ce6f565
Ines W <ineswright7@gmail.com>
2022-02-18 17:27:03 +0000

    [iiw20] Create return case class extending IRNode and include in code generator

--- src/main/scala/backend/IRGenerator.scala
+++ src/main/scala/backend/IRGenerator.scala
@@ -6,12 +6,18 @@ import java.io._
 
 object IRGenerator {
 
-  def generateIR(ast: WaccProgram): List[function] = {
-    ast.funcs.map(f => function(f.id.id, generateBody(f.body))) :+ function("main", generateBody(ast.stats))
+  def generateIR(ast: WaccProgram): List[FUNCTION] = {
+    ast.funcs.map(f => FUNCTION(f.id.id, generateBody(f.body))) :+ FUNCTION("main", generateBody(ast.stats) :+ RETURN())
   }
 
   def generateBody(stats: List[Stat]): List[IRNode] = {
-    Nil
+    stats match {
+        // TODO: generate assembly needed to evaluate expr, then insert something referring to that
+        // into the return IR node- perhaps an intermediate value with register in the symbol table
+      case List(Return(_)) => List(RETURN())
+      case _ :: t => generateBody(t)
+      case Nil => List.empty // WILL NEVER REACH HERE!
+    }
   }
 
   def generateCode(ast: WaccProgram, outputPath: String): Unit = {
--- src/main/scala/backend/intermediateRep.scala
+++ src/main/scala/backend/intermediateRep.scala
@@ -12,9 +12,12 @@ object intermediateRep {
   //TODO: load will need to change to put the return value in r0, not just always return 0
   // push {lr} stores the return address of where the function was called from
   // pop {pc} pulls that back out so control is returned to the caller
-  case class function(name: String, body: List[IRNode]) extends IRNode {
-    override def toString = name + ":\n\tPUSH {lr}\n" + body.mkString("\n") + "\tLDR r0, =0" +
+  case class FUNCTION(name: String, body: List[IRNode]) {
+    override def toString = name + ":\n\tPUSH {lr}\n" + body.mkString("\n") +
       "\n\tPOP {pc}\n\t.ltorg"
+  }
+  case class RETURN() extends IRNode {
+    override def toString = "\tLDR r0, =0"
   }
 
 }
