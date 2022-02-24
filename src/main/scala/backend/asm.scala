edf7c90a497fa678b3c17aadadcece933e0e53ff
Ines W <ineswright7@gmail.com>
2022-02-24 12:57:26 +0000

    [iiw20, raw20, ki20] Include toString representations of add, mul, push, pop

--- src/main/scala/backend/asm.scala
+++ src/main/scala/backend/asm.scala
@@ -11,15 +11,22 @@ object asm {
     override def toString = value + ":"
   }
 
-  case class Push(reg: String) extends Asm
-  case class Pop(reg: String) extends Asm
+  case class Push(reg: String) extends Asm {
+    override def toString = s"PUSH {$reg}"
+  }
+
+  case class Pop(reg: String) extends Asm {
+    override def toString = s"POP {$reg}"
+  }
 
   case class Or(target: String, x: String, y: String) extends Asm {
     def this(x: String, y: String) = this(x, x, y)
+    override def toString = s"ORR $target, $x, $y"
   }
 
   case class And(target: String, x: String, y: String) extends Asm {
     def this(x: String, y: String) = this(x, x, y)
+    override def toString = s"AND $target, $x, $y"
   }
 
   case class Eq(target: String, x: String, y: String) extends Asm {
@@ -48,14 +55,20 @@ object asm {
 
   case class Add(target: String, x: String, y: String) extends Asm {
     def this(x: String, y: String) = this(x, x, y)
+    override def toString = s"ADDS $target, $x, $y"
   }
 
   case class Sub(target: String, x: String, y: String) extends Asm {
+    //TODO: think about overflow errors
     def this(x: String, y: String) = this(x, x, y)
+    override def toString = s"SUBS $target, $x, $y"
   }
 
-  case class Mul(target: String, x: String, y: String) extends Asm {
-    def this(x: String, y: String) = this(x, x, y)
+  case class Mul(target1: String, target2: String, x: String, y: String) extends Asm {
+    def this(x: String, y: String) = this(x, y, x, y)
+    override def toString = s"SMULL $x, $y, $x, $y"
+    // TODO: include s"CMP $y, $x ASR #31\nBLNE ${label of overflow error function}"
+    // result will be in register x
   }
 
   case class Div(target: String, x: String, y: String) extends Asm {
