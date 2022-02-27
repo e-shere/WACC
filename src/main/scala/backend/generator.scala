c9f93184610480c6b1ca3060e63b43aa6812a9ef
Robert Weingart <raw20@imperial.ac.uk>
2022-02-27 21:12:32 +0000

    [es120, ki20, raw20] Rework step generation for more readability

--- src/main/scala/backend/generator.scala
+++ src/main/scala/backend/generator.scala
@@ -3,13 +3,17 @@ package backend
 import frontend.ast
 import frontend.ast._
 import asm._
+import asm.implicits._
 import state._
-import state.Step._
 import state.implicits._
 import frontend.symbols.TypeTable
 import scala.annotation.tailrec
 
 object generator {
+
+  private val r0 = AsmReg(0)
+  private val lr = AsmReg(14)
+  private val pc = AsmReg(13)
 
   // TODO: consider naming conventions for dynamically created unique labels
   private var uniqueNameGen = -1
@@ -25,11 +29,11 @@ object generator {
 
   // TODO: set sp
   def genMain(argc: Int, stats: List[Stat])(implicit symbols: TypeTable): Step = (
-    Label("main")
-    <++> Push("lr")
+         Label("main")
+    <++> Push(lr)
     <++> genStats(stats)
-    <++> Ldr("r0", "=0")()
-    <++> Pop("pc")
+    <++> Ldr(r0, AsmInt(0))
+    <++> Pop(pc)
     <++> Directive("ltorg")
     <++> Step.discard
   )
@@ -38,9 +42,9 @@ object generator {
   // Note that each ASM node here is implicitly converted to a step
   def genFunc(name: String, argc: Int, stats: List[Stat])(implicit symbols: TypeTable): Step = (
          Label(name)
-    <++> Push("lr")
+    <++> Push(lr)
     <++> genStats(stats)
-    <++> Pop("pc")
+    <++> Pop(pc)
     <++> Directive("ltorg")
     <++> Step.discard
   )
@@ -60,8 +64,8 @@ object generator {
       case Assign(lhs, rhs) => genRhs(rhs) <++> genLhs(lhs)
       case Read(lhs) => ???
       case Free(expr) => ??? // Need to find out if is pair or array
-      case Return(expr) => genExpr(expr) <++> r(reg => Mov(regToString(0), reg)())
-      case Exit(expr) => genExpr(expr) <++> r(reg => asm.Call(List(reg), "exit"))
+      case Return(expr) => genExpr(expr) <++> Mov.step(r0, _0)
+      case Exit(expr) => ??? // genExpr(expr) <++> r(reg => asm.Call(List(reg), "exit"))
       case Print(expr) => ???
       case Println(expr) => ???
       case s@If(expr, thenStats, elseStats) => {
@@ -69,66 +73,66 @@ object generator {
         val thenLabel = s"L_then_$l"
         val elseLabel = s"L_else_$l"
         val doneLabel = s"L_done_$l"
-        genExpr(expr) <++>
-        r(reg => Compare(reg, "#1")) <++>
-        Branch(thenLabel)("LEQ") <++>
-        Branch(elseLabel)("L") <++>
-        Branch(doneLabel)() <++>
-        Label("then") <++>
-        genStats(thenStats)(s.thenTypeTable.get) <++>
-        Label("else") <++>
-        genStats(elseStats)(s.elseTypeTable.get) <++>
-        Label("done")
+            (genExpr(expr)
+        <++> Compare.step(_0, AsmInt(1))
+        <++> Branch(thenLabel)("LEQ")
+        <++> Branch(elseLabel)("L")
+        <++> Branch(doneLabel)()
+        <++> Label("then")
+        <++> genStats(thenStats)(s.thenTypeTable.get)
+        <++> Label("else")
+        <++> genStats(elseStats)(s.elseTypeTable.get)
+        <++> Label("done"))
       }
       case s@While(expr, doStats) =>
         val l = getUniqueName
         val topLabel = s"L_while_cond_$l"
         val endLabel = s"L_while_end$l"
-        Label(topLabel) <++>
-        genExpr(expr) <++>
-        r(reg => Compare(reg, "#0")) <++>
-        Branch(endLabel)("EQ") <++>
-        genStats(doStats)(s.doTypeTable.get) <++>
-        Label(endLabel)
+            (Label(topLabel)
+        <++> genExpr(expr)
+        <++> Compare.step(_0, AsmInt(0))
+        <++> Branch(endLabel)("EQ")
+        <++> genStats(doStats)(s.doTypeTable.get)
+        <++> Label(endLabel))
       case s@Scope(stats) => genStats(stats)(s.typeTable.get)
     } 
   }
 
-  def genBinOp(x: Expr, y: Expr, f: (String, String) => Asm)(implicit symbols: TypeTable): Step = (
+  def genBinOp(x: Expr, y: Expr, step: Step)(implicit symbols: TypeTable): Step = (
            genExpr(x) 
       <++> genExpr(y)
-      <++> rro(f(_, _))
+      <++> step
     )
 
-  def genUnOp(x: Expr, f: (String) => Asm)(implicit symbols: TypeTable): Step = {
-    genExpr(x) <++> ro(f(_))
+  def genUnOp(x: Expr, step: Step)(implicit symbols: TypeTable): Step = {
+    genExpr(x) <++> step
   }
 
   def genExpr(expr: Expr)(implicit symbols: TypeTable): Step = {
     expr match {
-      case ast.Or(x, y)  => genBinOp(x, y, asm.Or(_, _)())
-      case ast.And(x, y) => genBinOp(x, y, asm.And(_, _)())
-      case ast.Eq(x, y)  => genBinOp(x, y, asm.Eq(_, _)())
-      case ast.Neq(x, y) => genBinOp(x, y, asm.Neq(_, _)())
-      case ast.Leq(x, y) => genBinOp(x, y, asm.Leq(_, _)())
-      case ast.Lt(x, y)  => genBinOp(x, y, asm.Lt(_, _)())
-      case ast.Geq(x, y) => genBinOp(x, y, asm.Geq(_, _)())
-      case ast.Gt(x, y)  => genBinOp(x, y, asm.Gt(_, _)())
-      case ast.Add(x, y) => genBinOp(x, y, asm.Add(_, _)())
-      case ast.Sub(x, y) => genBinOp(x, y, asm.Sub(_, _)())
-      case ast.Mul(x, y) => genBinOp(x, y, asm.Mul(_, _)())
-      case ast.Div(x, y) => genBinOp(x, y, asm.Div(_, _)())
-      case ast.Mod(x, y) => genBinOp(x, y, asm.Mod(_, _)())
-      case ast.Not(x)    => genUnOp(x, asm.Not(_)())
-      case ast.Neg(x)    => genUnOp(x, asm.Neg(_)())
-      case ast.Len(x)    => genUnOp(x, asm.Len(_)())
-      case ast.Ord(x)    => genUnOp(x, asm.Ord(_)())
-      case ast.Chr(x)    => genUnOp(x, asm.Chr(_)())
+      case ast.Or(x, y)  => genBinOp(x, y, asm.Or.step(_0, _0, _1))
+      case ast.And(x, y) => genBinOp(x, y, asm.And.step(_0, _0, _1))
+      case ast.Eq(x, y)  => genBinOp(x, y, asm.Eq.step(_0, _0, _1))
+      case ast.Neq(x, y) => genBinOp(x, y, asm.Neq.step(_0, _0, _1))
+      case ast.Leq(x, y) => genBinOp(x, y, asm.Leq.step(_0, _0, _1))
+      case ast.Lt(x, y)  => genBinOp(x, y, asm.Lt.step(_0, _0, _1))
+      case ast.Geq(x, y) => genBinOp(x, y, asm.Geq.step(_0, _0, _1))
+      case ast.Gt(x, y)  => genBinOp(x, y, asm.Gt.step(_0, _0, _1))
+      case ast.Add(x, y) => genBinOp(x, y, asm.Add.step(_0, _0, _1))
+      case ast.Sub(x, y) => genBinOp(x, y, asm.Sub.step(_0, _0, _1))
+      case ast.Mul(x, y) => genBinOp(x, y, asm.Mul.step(_0, _0, _1))
+      //case ast.Div(x, y) => genBinOp(x, y, asm.Div.step(_0, _0, _1))
+      //case ast.Mod(x, y) => genBinOp(x, y, asm.Mod.step(_0, _0, _1))
+      case ast.Not(x)    => genUnOp(x, asm.Not.step(_0, _0))
+      case ast.Neg(x)    => genUnOp(x, asm.Neg.step(_0, _0))
+      case ast.Len(x)    => genUnOp(x, asm.Len.step(_0, _0))
+      case ast.Ord(x)    => genUnOp(x, asm.Ord.step(_0, _0))
+      case ast.Chr(x)    => genUnOp(x, asm.Chr.step(_0, _0))
       // TODO: deal with int overflow
-      case ast.IntLiter(x) => w(reg => Ldr(reg, s"=$x")())
-      case ast.BoolLiter(x) => w(reg => Ldr(reg, intToAsmLit(x.compare(false)))())
+      case ast.IntLiter(x) => Ldr.step(_0, AsmInt(x))
+      case ast.BoolLiter(x) => Ldr.step(_0, AsmInt(x.compare(false)))
         // TODO: let ldr take a char directly
-      case ast.CharLiter(x) => w(reg => Ldr(reg, intToAsmLit(x.toInt))())
+      case ast.CharLiter(x) => Ldr.step(_0, AsmInt(x.toInt))
       case ast.StrLiter(x) => ???
       case ast.ArrayLiter(x) => ???
       case ArrayElem(id, index) => ???
@@ -143,38 +147,35 @@ object generator {
     rhs match {
         // [0,5,7,2]
       case ArrayLiter(exprs) => (
-             w(Mov(_, intToAsmLit(exprs.length))())
-        <++> w(Mov(_, intToAsmLit((exprs.length + 1) * 4))())
-        <++> ro(reg => asm.Call(List(reg), "malloc")) // replace sizeInBytes with a pointer to the array
-        <++> rro(
-          Str(_, _)(), // Store sizeInElements in array[0]
-          Mov(_, _)() // replace sizeInElements with array pointer
-        )
+             Mov.step(_0, AsmInt(exprs.length))
+        <++> Mov.step(_0, AsmInt((exprs.length + 1) * 4))
+        <++> genCallWithRegs("malloc", 1) // replace sizeInBytes with a pointer to the array
+        <++> Str.step(_0, _1) // TODO: avoid this register leak (the bottom register isn't used again)
              // -> size, ------
              // -> pointer to array, nothing
         <++> exprs.zipWithIndex.foldLeft(Step.identity)((prev, v) => (
                prev 
           <++> genExpr(v._1) // put value in a register
                  // TODO: intToOffset
-          <++> rro((pos, value) => Str(value, pos)(s"#${(v._2 + 1) * 4}")) // store value at pos, pos remains on the stack
+          <++> Str.step(_1, _0, AsmInt((v._2 + 1) * 4)) // store value at pos, pos remains on the stack
         ))
       )
       case NewPair(fst, snd) => (
-             w(Mov(_, intToAsmLit(4 * 2))())
-        <++> ro(reg => asm.Call(List(reg), "malloc"))
+             Mov.step(_0, AsmInt(4 * 2))
+        <++> genCallWithRegs("malloc", 1)
         <++> genExpr(fst)
-        <++> rro((pos, value) => Str(value, pos)())
+        <++> Str.step(_1, _0)
         <++> genExpr(snd)
              // TODO: intToOffset
-        <++> rro((pos, value) => Str(value, pos)(s"#4"))
+        <++> Str.step(_1, _0, AsmInt(4))
       )
       case Fst(expr) => (
              genExpr(expr)
-        <++> ro(reg => Ldr(reg, reg)())
+        <++> Ldr.step(_0, _0)
       )
       case Snd(expr) => (
              genExpr(expr)
-        <++> ro(reg => Ldr(reg, reg)(s"#4"))
+        <++> Ldr.step(_0, _0, AsmInt(4))
       )
       case ast.Call(id, args) => //TODO: a variable number of reads into the Nil
           args.foldLeft(Step.identity)(_ <++> genExpr(_)) //<++>
@@ -186,13 +187,13 @@ object generator {
   def genLhs(lhs: AssignLhs)(implicit symbols: TypeTable): Step = lhs match {
     case id@Ident(_) => {
       val offset = countToOffset(symbols.getOffset(id).get)
-      r(reg => Str(reg, STACK_POINTER)(s"#$offset"))
+      Str.step(_0, STACK_POINTER, AsmInt(offset)) // TODO: this leaks a register
       // TODO: account for movement in stack pointer
     }
     case ArrayElem(id, index) => {
       // this is the case a[3] = x
       val offset = countToOffset(symbols.getOffset(id).get)
-      genExpr(index) <++> r(reg => Str(reg, STACK_POINTER)(s"#${offset + 1}"))
+      genExpr(index) <++> Str.step(_0, STACK_POINTER, AsmInt(offset + 1))
 
       // nodes
       // r? = index + offset
@@ -207,4 +208,13 @@ object generator {
       ???
     }
   }
+
+  def genCallWithRegs(name: String, argc: Int): Step = {
+    assert(argc >= 0 && argc <= 4)
+    (0 until argc).reverse.foldLeft(Step.identity)((prev, num) => {
+      prev <++> Mov.step(AsmReg(num), _0)
+    })
+    Branch(name)("L")
+  }
 }
+
