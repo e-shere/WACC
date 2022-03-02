93d19c9d227d1df323cefc06f53f2ee9b38fe2f5
Ines W <ineswright7@gmail.com>
2022-03-02 18:52:43 +0000

    [iiw20, raw20] Update ident generation to use AsmStateFunc

--- src/main/scala/backend/generator.scala
+++ src/main/scala/backend/generator.scala
@@ -168,13 +168,9 @@ object generator {
 // TODO:       <++> asm.Mul.step(_0, _0, AsmInt(BYTE_SIZE))
         >++> Step.asmInstr(asm.Adds())(Re2, Re2, Re1)(Re2)
         )
-      case idd@Ident(id) => Step({ state =>
-        val offset = countToOffset(symbols.getOffset(idd).get + state.getStackOffset)
-        Step.genericAsmInstr(asm.Ldr())(ReNew, STACK_POINTER)(AsmInt(offset))()(state)
-        // Ldr.step(_0, STACK_POINTER, AsmInt(offset))
-      })
+      case idd@Ident(_) => genLocation(idd) >++> Step.genericAsmInstr(asm.Ldr())(ReNew, Re1)(AsmInt(0))()
       case Null() => ???
-      case Paren(expr) => genExpr(expr) // same symbol table?
+      case Paren(expr) => genExpr(expr)
     }
   }
 
@@ -182,7 +178,7 @@ object generator {
   def genRhs(rhs: AssignRhs)(implicit symbols: TypeTable): Step = {
     rhs match {
         // [0,5,7,2]
-      case arr@ArrayLiter(exprs) => genExpr(arr)
+      case arr@ArrayLiter(_) => genExpr(arr)
       case NewPair(fst, snd) => (
         Step.asmInstr(asm.Mov())(Re1, AsmInt(4 * 2))(Re1)
         >++> genCallWithRegs("malloc", 1, Some(r0))
@@ -220,9 +216,11 @@ object generator {
   // puts the memory location of the object in question in a register
   def genLocation(lhs: AssignLhs)(implicit symbols: TypeTable): Step = lhs match {
     case id@Ident(_) => Step({state =>
-      val offset = countToOffset(symbols.getOffset(id).get + state.getStackOffset)
       // This stores the actual location in a new register
-      Step.asmInstr(asm.Adds())(ReNew, STACK_POINTER, AsmInt(offset))()(state)
+      (Step.asmInstr(asm.Adds())
+      (ReNew, STACK_POINTER, AsmStateFunc(
+        (s: State) => AsmInt(countToOffset(symbols.getOffset(id).get + s.getStackOffset))))
+      ()(state))
     })
     case ArrayElem(id, index) => (
            genExpr(id)
