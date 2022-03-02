c94c21934f89912409c7771433960f89aac54711
Ines W <ineswright7@gmail.com>
2022-03-02 10:01:00 +0000

    [iiw20] Include asserts in state

--- src/main/scala/backend/state.scala
+++ src/main/scala/backend/state.scala
@@ -37,23 +37,27 @@ object state {
     def next: State = State(AsmReg(reg.r + 1), this.fState)
 
     def read: (AsmReg, List[Asm], State) = {
+      assert(reg.r > 4)
       if (prev.isReg) (prev.reg, Nil, prev)
       else (PLACEHOLDER_1, List(Pop()(PLACEHOLDER_1)), prev)
     }
 
     def read2: (AsmReg, AsmReg, List[Asm], State) = {
+      assert(reg.r > 5)
       if (isReg) (prev.reg, reg, Nil, prev.prev)
       else if (prev.isReg) (prev.reg, PLACEHOLDER_1, List(Pop()(PLACEHOLDER_1)), prev.prev)
       else (PLACEHOLDER_1, PLACEHOLDER_2, List(Pop()(PLACEHOLDER_2), Pop()(PLACEHOLDER_1)), prev.prev)
     }
 
     def peek: (AsmReg, List[Asm], State) = {
+      assert(reg.r > 4)
       if (isReg) (prev.reg, Nil, this)
       // TODO: added 0 offset here. Check
       else (PLACEHOLDER_1, List(Ldr()(PLACEHOLDER_1, STACK_POINTER)(AsmInt(0))), this)
     }
 
     def peek2: (AsmReg, AsmReg, List[Asm], State) = {
+      assert(reg.r > 5)
       if (isReg) (prev.reg, reg, Nil, this)
       // TODO: added 0 offset here. Check
       else if (prev.isReg) (prev.reg, PLACEHOLDER_1, List(Ldr()(PLACEHOLDER_1, STACK_POINTER)(AsmInt(0))), this)
