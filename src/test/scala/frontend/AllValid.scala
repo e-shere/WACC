d379d8d627d7ad732adc14a4c3238a910216d8dc
Ines W <ineswright7@gmail.com>
2022-03-03 11:00:25 +0000

    Resolve compilation errors from merge conflicts

--- src/test/scala/frontend/AllValid.scala
+++ src/test/scala/frontend/AllValid.scala
@@ -20,7 +20,8 @@ class AllValid extends AnyFlatSpec {
         .filter(_.isDirectory)
         .toList
         .flatMap(x => getListOfFilesRecursively(x.toString()))
-      d.listFiles.filter(_.isFile).map(_.toString()).concat(result)
+      d.listFiles.filter(_.isFile).filter(_.toString.endsWith(".wacc"))
+        .map(_.toString()).concat(result)
     } else {
       Array[String]()
     }
