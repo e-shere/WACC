5cc65d8091048f5e5fb2e519a63cdbf6bcaaebe1
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-03-04 02:13:29 +0000

    [es120] Add overflow checking in Add and Sub

--- src/main/scala/backend/asm.scala
+++ src/main/scala/backend/asm.scala
@@ -1,6 +1,6 @@
 package backend
 
-import backend.asm.ConditionCode.{AL, EQ, GE, GT, LE, LT, NE}
+import backend.asm.ConditionCode.{AL, EQ, GE, GT, LE, LT, NE, VS}
 import backend.state.State
 import backend.step._
 import backend.step.implicits._
@@ -114,7 +114,7 @@ object asm {
 
   object ConditionCode extends Enumeration {
     type Cond = Value
-    val EQ, NE, LT, LE, GT, GE, CS = Value
+    val EQ, NE, LT, LE, GT, GE, CS, VS = Value
     val AL: ConditionCode.Value = Value("")
   }
 
